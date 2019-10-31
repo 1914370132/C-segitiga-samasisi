b37e836cff3b988d0aca3e3f9ee6c907862658e5
M.irpan subki bangko <55613674+1914370132@users.noreply.github.com>
2019-10-31 13:10:48 +0700

    Add files via upload

---
+++ Irpan.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    printf("\   WELCOME   \n");
+    printf("\      TO       \n");
+    printf("\    UNPAB      \n");
+    printf("\tBIODATA PRIBADI SAYA\n\n");
+    printf("NAMA : IRPAN SUBKI BANGKO\n");
+    printf("\NPM : 1914370132\n");
+    printf("\KELAS : REGULER 1D\n");
+    printf("\FAKULTAS : SAINS & TEKNOLOGI\n");
+    printf("\PRODI : SISKOM\n");
+
+
+    int a=180;
+    int b=20;
+    int c;
+    c=a-b;
+    printf("baris 1 -nilai c adalah %d\n",c);
+    c=a+b;
+    printf("baris 2 -nilai c adalah %d\n",c);
+    c=a*b;
+    printf("baris 3 -nilai c adalah %d\n",c);
+    c=a%b;
+    printf("baris 4 -nilai c adalah %d\n",c);
+    c=a/b;
+    printf("baris 5 -nilai c adalah %d\n",c);
+    c=a++;
+    printf("baris 6 -nilai c adalah %d\n",c);
+    c=a--;
+    printf("baris 7 -nilai c adalah %d\n",c);
+    printf("\THANK YOU\n");
+    return 0;
+}
